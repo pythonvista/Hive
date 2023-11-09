4680d1dd4501505ad3ce2b999a9a3b35b0c4fd97
pythonvista <emeterv18@gmail.com>
2023-11-09 22:41:29 +0100

    updated

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
  
- <div class="h-[83vh] overflow-hidden overflow-y-scroll flex flex-col items-center gap-4  justify-center ">
+ <div class="h-[88vh] overflow-hidden overflow-y-scroll flex flex-col items-center gap-4  justify-center ">
   <img class="w-[70%]" src="@/assets/img/logo_full.png" alt="">
   <HomeLanguageSelect></HomeLanguageSelect>
  </div>
