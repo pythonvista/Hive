9050dc8e9f7fff337f9a7eeda65c52c9b6e70b10
pythonvista <emeterv18@gmail.com>
2023-11-09 22:34:22 +0100

    updated

--- components/Home/LanguageSelect.vue
+++ components/Home/LanguageSelect.vue
@@ -1,33 +1,31 @@
 <template>
   <div class="w-full px-5">
     <UtilsHeading :title="'Select Language'"></UtilsHeading>
-    <div class="mb-2 py-3 px-3 mt-3 gap-4 flex rounded-lg border border-gray-100 bg-white text-sm font-medium shadow-sm hover:border-gray-200 peer-checked:border-blue-500 peer-checked:ring-1 peer-checked:ring-blue-500" v-for="(lan, index) in langs" :key="index">
-      <input
-        type="radio"
-        name="language"
-        class="peer "
-      />
-
+    <div @click="lang = lan.value" :class="{'border-2 border-solid border-[#6f64f0]': lan.value == lang, 'border-2 border-gray-100': lan.value != lang}"
+      class="mb-2   py-3 px-3 mt-3 gap-4 flex rounded-lg  bg-white text-sm font-medium shadow-sm "
+      v-for="(lan, index) in langs"
+      :key="index"
+    >
      
-       
-
-        <p class="text-gray-900">
-        {{lan}}
-        </p>
-  
+      <q-radio v-model="lang" color="#6f64f0" :val="lan.value" :label="lan.label" />
+     
     </div>
-    <div class="flex justify-center items-center">
-        <q-btn >Continue</q-btn>
+    <div class="flex mt-3 justify-center items-center">
+      <q-btn color="#6f64f0" class="bg-[#6f64f0]">Continue</q-btn>
     </div>
-   
   </div>
 </template>
 
 <script>
 export default {
   data: () => ({
-    lang: 'English',
-    langs: ['English', 'Yoruba', 'Hausa', 'Igbo'],
+    lang: 'eng',
+    langs: [
+      { label: 'English', value: 'eng' },
+      { label: 'Yoruba', value: 'yor' },
+      { label: 'Hausa', value: 'hau' },
+      { label: 'Igbo', value: 'igb' },
+    ],
   }),
 };
 </script>
