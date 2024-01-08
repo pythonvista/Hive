f50f74cedb68a0e606f21a4a0e1250a907475638
pythonvista <nablintech@gmail.com>
2024-01-08 09:17:29 -0800

    sd

--- components/Utils/LocationSlider.vue
+++ components/Utils/LocationSlider.vue
@@ -1,17 +1,19 @@
 <template>
     <div class=" py-3">
-        v class="mt-4 py-3">
-           <div class="flex justify-between items-center">
-                  <p class="font-bold text-lg text-slate-600">Popular Locations</p>
-                  <div>
-                     <q-btn color="blue" size="18px" flat icon="list"></q-btn>
-                  </div>
-     
-
+        <div class="mt-4 py-3">
+            <div class="flex justify-between items-center">
+                <p class="font-bold text-lg text-slate-600">Popular Locations</p>
+                <div>
+                    <q-btn color="blue" size="18px" flat icon="list"></q-btn>
                 </div>
+
+
+            </div>
+        </div>
         <swiper :slidesPerView="2" :spaceBetween="10" :modules="modules" class="mySwiper">
             <swiper-slide v-for="i in 20" :key="i" class="py-1">
-                <div @click="$router.push({path: '/view_location'})" class="relative flex h-[240px] p-1 flex-col w-full overflow-hidden bg-gray-100 shadow group rounded-2xl">
+                <div @click="$router.push({ path: '/view_location' })"
+                    class="relative flex h-[240px] p-1 flex-col w-full overflow-hidden bg-gray-100 shadow group rounded-2xl">
                     <div class=" rounded-2xl  h-[83%]  overflow-hidden ">
                         <img class="object-contain w-full h-full transition-all duration-300 transform group-hover:scale-125"
                             src="https://i.imgur.com/v0A01uI.jpg" alt="" />
